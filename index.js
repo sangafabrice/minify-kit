81416f2be08b0beb319f2eb2b8149a5ec3bb0d15
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2025-12-15 05:21:04 +0100

    refactor(demo): remove type definition

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ import minifyCSS from "./minify/css.js";
 import minifySVG from "./minify/svg.js";
 import minifyJS from "./minify/js.js";
 
-type Extension = ".css" | ".html" | ".js" | ".svg";
-
 /**
  * Minify file content based on its extension.
  * @alias minify
