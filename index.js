050d5625b5309dc26de994fd7c1d3793eedc6fe6
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2025-12-18 09:35:09 +0100

    refactor(demo): add application-level flow files

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-/** @flow */
 import minifyHTML from "./minify/html/index.js";
 import minifyCSS from "./minify/css.js";
 import minifySVG from "./minify/svg.js";
 import minifyJS from "./minify/js.js";
 
-/**
- * Minify file content based on its extension.
- * @alias minify
- * @param extname - The file extension.
- * @param content - Raw file content to minify.
- * @returns A promise that resolves to the minified content
- */
-export default async function(extname/* : Extension */, content/* : string */)/* : Promise<string> */ {
+export default async function(extname, content) {
     switch (extname) {
         case ".css": return minifyCSS(content);
         case ".svg": return minifySVG(content);
