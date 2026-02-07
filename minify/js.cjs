f25c2be09d3e0614eb49bea168257cac98c27e51
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-02-07 05:05:25 +0100

    chore: simplify commonjs script

--- minify/js.cjs
+++
@@ -1,9 +0,0 @@
-const { minify } = require("terser");
-
-function minifyJS(content) {
-    return minify(content, { module: true }).then(
-        ({ code = "" }) => code
-    );
-}
-
-module.exports = Object.freeze(minifyJS);
--- minify/svg.cjs
+++
@@ -1,7 +0,0 @@
-const { optimize } = require("svgo");
-
-function minifySVG(content) {
-    return optimize(content, { multipass: true }).data;
-}
-
-module.exports = Object.freeze(minifySVG);
